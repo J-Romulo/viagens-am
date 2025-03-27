e5781cb3811f3799763957c5e2ef02627bb60738
J-Romulo <joseromulo.10@hotmail.com>
2025-03-27 21:16:22 +1300

    feat: add api and auth utils

---
+++ src/utils/auth.ts
@@ -0,0 +1,18 @@
+"use server";
+import { cookies } from "next/headers";
+
+export async function setAuthCookie(token: string) {
+  const oneDayInFuture = new Date();
+
+  oneDayInFuture.setDate(oneDayInFuture.getDate() + 1);
+
+  const cookieStore = await cookies();
+  cookieStore.set("token", token, {
+    expires: oneDayInFuture,
+  });
+}
+
+export async function deleteAuthCookie() {
+  const cookieStore = await cookies();
+  cookieStore.delete("token");
+}
