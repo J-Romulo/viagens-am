bddfd3a786961437d3609f01d21572a13a7627a0
J-Romulo <joseromulo.10@hotmail.com>
2025-03-28 21:50:56 +1300

    feat: finish login page logic

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -3,7 +3,6 @@
 import { isAxiosError } from "axios";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { FormEvent, useState } from "react";
 import api from "../../services/api";
 import { setAuthCookie } from "../../utils/auth";
 import { toast } from "react-toastify";
@@ -11,17 +10,38 @@ import Link from "next/link";
 import { Button } from "../../components/Button";
 import { TextInput } from "../../components/TextInput";
 import { PasswordInput } from "../../components/PasswordInput";
+import { useForm } from "@tanstack/react-form";
+import { z } from "zod";
+import Loader from "react-spinners/ClipLoader";
+
+const signInSchema = z.object({
+    email: z.string()
+      .email("Email inválido")
+      .min(1, "Email é obrigatório"),
+    
+    password: z.string()
+      .min(6, "A senha deve ter no mínimo 6 caracteres")
+      .max(256, "A senha deve ter no máximo 256 caracteres")
+});
 
 export default function SignIn() {
+    const form = useForm({
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+        onSubmit: async ({ value }) => {
+            await handleSubmit(value);
+        },
+        validators: {
+            onChange: signInSchema,
+        },
+    })
     const router = useRouter();
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-
-    async function handleSubmit(e: FormEvent) {
-        e.preventDefault();
+    async function handleSubmit(data: {email: string; password: string; }) {
         try {
-            const response = await api.post("auth/sign-in", { email, password });
+            const response = await api.post("auth/sign-in", { email: data.email, password: data.password });
             const token = response.data.token;
 
             localStorage.setItem("username", response.data.user.name);
@@ -46,29 +66,69 @@ export default function SignIn() {
                 <Image src={"/"} alt="Viagens AM logo" width={120} height={200} />
             </div>
 
-            <form className="space-y-6 mx-auto mt-6 w-2/3" onSubmit={handleSubmit}>
-                <TextInput 
-                    id="email"
-                    label="Email"
-                    type="email"
-                    onChange={(text) => setEmail(text)}
-                    placeholder="Digite sua email"
-                    required={true}
-                />
-                
-                <PasswordInput
-                    id="password"
-                    label="Senha"
-                    onChange={(text) => setPassword(text)}
-                    placeholder="Digite sua senha"
-                    required={true}
-                />
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault()
+                    e.stopPropagation()
+                    form.handleSubmit()
+                }}
+                className="w-full flex flex-col items-center justify-center"
+            >
+                <div className="space-y-6 mx-auto mt-6 w-2/3">
+                    <form.Field
+                        name="email"
+                    >
+                        {(field) => {
+                            return (
+                                <TextInput
+                                    id={field.name}
+                                    label="Email"
+                                    type="email"
+                                    value={field.state.value}
+                                    onChange={(text) => field.handleChange(text)}
+                                    placeholder="Digite sua email"
+                                    ringColor={field.state.meta.errors.length ? "accent-400" : "primary-400"}
+                                    required={true}
+                                />
+                            )
+                        }}
+                    </form.Field>
+                    
+                    <form.Field
+                        name="password"
+                    >
+                        {(field) => {
+                            return (
+                                <PasswordInput
+                                    id={field.name}
+                                    label="Senha"
+                                    value={field.state.value}
+                                    onChange={(text) => field.handleChange(text)}
+                                    placeholder="Digite sua senha"
+                                    ringColor={field.state.meta.errors.length ? "accent-400" : "primary-400"}
+                                    required={true}
+                                />
+                            )
+                        }}
+                    </form.Field>
 
-                <Button 
-                    type="submit"
-                >
-                    Entrar
-                </Button>
+                    <form.Subscribe selector={(state) => [state.canSubmit, state.isSubmitting]}>
+                        {([canSubmit, isSubmitting]) => (
+                            <Button 
+                                type="submit"
+                                disabled={!canSubmit}
+                            >
+                                {isSubmitting ?
+                                    <Loader
+                                        color={"#FFF"}
+                                        loading={isSubmitting}
+                                        size={20}
+                                    />
+                                : 'Entrar'}
+                            </Button>
+                        )}
+                    </form.Subscribe>
+                </div>
             </form>
 
             <p className="text-center text-neutral-700 mt-4 mx-auto">
