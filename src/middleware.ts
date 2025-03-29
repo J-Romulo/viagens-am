7872493698f713142a03a22468a4fc49721c575e
J-Romulo <joseromulo.10@hotmail.com>
2025-03-29 22:41:11 +1300

    feat: add middleware to protect routes

---
+++ src/middleware.ts
@@ -0,0 +1,21 @@
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+const protectedRoutes = ["/home", "/", "/settings"];
+
+export default async function middleware(req: NextRequest) {
+  const path = req.nextUrl.pathname;
+  const isProtectedRoute = protectedRoutes.includes(path);
+
+  if (path === "/") {
+    return NextResponse.redirect(new URL("/home", req.nextUrl));
+  }
+
+  const token = (await cookies()).get("token")?.value;
+
+  if (isProtectedRoute && !token) {
+    return NextResponse.redirect(new URL("/signIn", req.nextUrl));
+  }
+
+  return NextResponse.next();
+}
