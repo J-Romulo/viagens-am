c6e7982b5c2e00a1d97ccfd2fa1085f532cea27f
J-Romulo <joseromulo.10@hotmail.com>
2025-04-02 21:02:17 +1300

    feat: add table component

--- src/app/(dashboard)/trips/page.tsx
+++ src/app/(dashboard)/trips/page.tsx
@@ -7,36 +7,86 @@ import { Trip } from "../../../@types/Trip";
 import Loader from "react-spinners/ClipLoader";
 import { toast } from "react-toastify";
 import { useEffect } from "react";
+import { createColumnHelper } from "@tanstack/react-table";
+import { Table } from "../../../components/Table";
+
+const columnHelper = createColumnHelper<Trip>()
+
+const columns = [
+  columnHelper.accessor('city', {
+    header: "Cidade",
+    cell: info => info.getValue(),
+  }),
+  columnHelper.accessor('uf', {
+    header: "Estado",
+    cell: info => info.getValue(),
+  }),
+  columnHelper.accessor('hotel', {
+    header: "Hotel",
+    cell: info => info.getValue(),
+  }),
+  columnHelper.accessor('start_date', {
+    header: "Ida",
+    cell: (info) => Intl.DateTimeFormat('pt-BR', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false, // 24-hour format
+    })
+      .format(new Date(info.getValue()))
+      .replace(',', ' às'),
+  }),
+  columnHelper.accessor('finish_date', {
+    header: "Volta",
+    cell: (info) => Intl.DateTimeFormat('pt-BR', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false, // 24-hour format
+    })
+      .format(new Date(info.getValue()))
+      .replace(',', ' às'),
+  }),
+]
 
 export default function Trips() {
-    const tripsQuery = useQuery<Trip[]>({
-        queryKey: ['trips'],
-        queryFn: getUserTrips,
-        refetchOnWindowFocus: false
-    });
+  const tripsQuery = useQuery<Trip[]>({
+    queryKey: ['trips'],
+    queryFn: getUserTrips,
+    refetchOnWindowFocus: false
+  });
 
-    useEffect(() => {
-        if (tripsQuery.isError) {
-            toast.error("Ocorreu um erro ao tentar buscar as viagens. Tente novamente em instantes.");
-
-        }
-    }, [tripsQuery.isError]);
-
-    if(tripsQuery.isLoading) {
-        return (
-            <div className="flex flex-col w-full h-full items-center bg-white shadow-lg rounded-lg p-3">
-                <PageTitle title="Viagens" />
-                <Loader
-                    color={"#4f46e5"}
-                    loading={true}
-                    size={60}
-                />
-            </div>
-        )
+  useEffect(() => {
+    if (tripsQuery.isError) {
+      toast.error("Ocorreu um erro ao tentar buscar as viagens. Tente novamente em instantes.");
     }
+  }, [tripsQuery.isError]);
+        
+  if(tripsQuery.isLoading) {
     return (
-        <div className="flex flex-col bg-white shadow-lg rounded-lg p-3 w-full h-full">
-            <PageTitle title="Viagens" />
-        </div>
+      <div className="flex flex-col w-full h-full items-center bg-white shadow-lg rounded-lg p-3">
+          <PageTitle title="Viagens" />
+          <Loader
+              color={"#4f46e5"}
+              loading={true}
+              size={60}
+              />
+      </div>
     )
+  }
+
+  return (
+      <div className="flex flex-col bg-white shadow-lg rounded-lg p-3 w-full h-full">
+          <PageTitle title="Viagens" />
+
+          <Table 
+            data={tripsQuery.data}
+            columns={columns}
+          />
+      </div>
+  )
 }
---
+++ src/components/Table/index.tsx
@@ -0,0 +1,60 @@
+import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table"
+export interface TableProps<T> {
+	data?: T[];
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    columns: ColumnDef<T, any>[];
+}
+
+export function Table<T>(props: TableProps<T>) {
+    const {
+		data,
+		columns
+	} = props;
+
+    const table = useReactTable({
+        data: data || [],
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+    })
+    
+    return (
+        <table className="w-full border-collapse rounded-lg overflow-hidden">
+            <thead className="bg-primary-500 text-white">
+                {table.getHeaderGroups().map(headerGroup => (
+                <tr key={headerGroup.id}>
+                    {headerGroup.headers.map(header => (
+                    <th 
+                        key={header.id} 
+                        className="py-3 px-4 text-left font-medium border-b border-primary-600"
+                    >
+                        {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                            )}
+                    </th>
+                    ))}
+                </tr>
+                ))}
+            </thead>
+            <tbody>
+                {table.getRowModel().rows.map((row, index) => (
+                <tr 
+                    key={row.id} 
+                    className={index % 2 === 0 ? 'bg-white' : 'bg-neutral-100'}
+                >
+                    {row.getVisibleCells().map(cell => (
+                    <td 
+                        key={cell.id} 
+                        className="py-3 px-4 border-b border-neutral-300 text-neutral-500"
+                    >
+                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </td>
+                    ))}
+                </tr>
+                ))}
+            </tbody>
+        </table>
+    )
+}
