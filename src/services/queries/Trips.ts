f567e7acf23859c0b0b135f2bd43b606c14fc622
J-Romulo <joseromulo.10@hotmail.com>
2025-04-13 13:54:34 +1200

    feat: add query to create trip

--- src/services/queries/Trips.ts
+++ src/services/queries/Trips.ts
@@ -8,3 +8,13 @@ export async function getUserTrips(): Promise<Trip[]> {
 
 	return data;
 }
+
+type CreateTrip = Omit<Trip, "_id" | "user_id" | "clients" | "created_at" | "updated_at">;
+
+export async function createTrip(trip: CreateTrip): Promise<Trip> {
+	const { data } = await api.post(
+		`/trips`,
+		trip
+	);
+	return data;
+}
