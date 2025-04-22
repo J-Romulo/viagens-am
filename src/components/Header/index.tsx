231b1d531e2e8d8d80fa022b1f39eecd93726f16
J-Romulo <joseromulo.10@hotmail.com>
2025-04-22 18:35:10 +1200

    feat: add avatar and logo in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,22 @@
 "use client";
 
 import Image from "next/image";
-import AMLogo from "../../assets/am-logo.png";
 import { CustomPopover } from "../Popover";
 import { use, useState } from "react";
 import { ProfileMenu } from "./ProfileMenu";
 import { AuthContext } from "../../Contexts/AuthContext";
+import AMLogoDark from "../../assets/am-logo-dark.png"
 
 export function Header() {
     const { user } = use(AuthContext);
     
     const [isProfileMenuOpened, setIsProfileMenuOpened] = useState(false);
 
-    console.log(user)
     return (
         <div className="flex items-center justify-between w-full h-15 p-4 bg-primary-500 text-white shadow-lg">
-            <div></div>
+            <div>
+                <Image src={AMLogoDark} alt="AM Viagens logo" width={100} height={150} unoptimized/>
+            </div>
             <div className="flex items-center gap-x-3">
                 <p>
                     { user?.name }
@@ -26,13 +27,23 @@ export function Header() {
 					onClose={() => setIsProfileMenuOpened(false)}
                     content={<ProfileMenu />}
                 >
-                    <Image 
-                        src={AMLogo}
-                        alt="AM Viajens logo"
-                        className="rounded-full w-13 h-13 object-contain bg-primary-100 cursor-pointer"
-                        unoptimized
-                        onClick={() => setIsProfileMenuOpened(!isProfileMenuOpened)}
-                    />
+                    {user?.avatar ? (
+                        <Image 
+                            src={`${process.env.NEXT_PUBLIC_API_URL}/images/avatars/${user?.avatar}`}
+                            alt="Imagem de perfil"
+                            className="rounded-full w-13 h-13 object-contain bg-white cursor-pointer"
+                            unoptimized
+                            width={13}
+                            height={13}
+                            onClick={() => setIsProfileMenuOpened(!isProfileMenuOpened)}
+                        />
+                    ) : (
+                        <div className="w-full h-full rounded-full bg-primary-100 flex items-center justify-center">
+                        <span className="text-4xl font-bold text-primary-500">
+                            {user?.name.charAt(0).toUpperCase()}
+                        </span>
+                        </div>
+                    )}
                 </CustomPopover>
             </div>
         </div>
