8fd9587219a3004fdc734ca51b3e42cc405ab35b
J-Romulo <joseromulo.10@hotmail.com>
2025-04-25 17:29:01 +1200

    chore: remove lint command from lint-staged configuration and fix formatting in Traveler type definition

--- src/@types/Traveler.ts
+++ src/@types/Traveler.ts
@@ -5,11 +5,11 @@ export type Traveler = {
   user_id: string;
   full_name: string;
   cpf: string;
-                                    rg: string;
-                                    birth_date: Date;
-                                    email?: string;
-                                    phone?: string;
-                                    trips?: Trip[];
-                                    created_at: Date;
-                                    updated_at: Date;
+  rg: string;
+  birth_date: Date;
+  email?: string;
+  phone?: string;
+  trips?: Trip[];
+  created_at: Date;
+  updated_at: Date;
 };
