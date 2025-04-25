9326b45abaab8843985201b7f64996c1e2776504
J-Romulo <joseromulo.10@hotmail.com>
2025-04-25 15:33:06 +1200

    fix: update CreateTrip type to include 'rooms' field for trip creation

--- src/services/queries/Trips.ts
+++ src/services/queries/Trips.ts
@@ -17,7 +17,7 @@ export async function getTripById(id: string): Promise<Trip> {
 	return data;
 }
 
-type CreateTrip = Omit<Trip, "_id" | "user_id" | "clients" | "created_at" | "updated_at">;
+type CreateTrip = Omit<Trip, "_id" | "user_id" | "clients" | "created_at" | "updated_at" | "rooms">;
 
 export async function createTrip(trip: CreateTrip): Promise<Trip> {
 	const { data } = await api.post(
